6b24718b54fde8bb191759ffe25756c0f85bfab8
manyports <157960666+manyports@users.noreply.github.com>
2024-03-03 12:59:25 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import Header from "./Header.jsx"
 import Content from "./Content.jsx"
+import Time from './Time';
+import Footer from './Footer.jsx'
 function App() {
 
   return (
     <>
      <Header/>
      <Content/>
+     <Time/>
+     <Footer/>
     </>
   )
 }
-
 export default App
